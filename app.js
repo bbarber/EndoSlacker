5d18e1022a920ec50d1aab80030d0577c0cafb24
Branden Barber <bbarber@nebraskaglobal.com>
2016-02-10 11:27:19 -0600

    Cleanup + fix dom parse

--- app.js
+++ app.js
@@ -5,30 +5,40 @@ var request = require('request');
 var secrets = require('./secrets');
 var app = express();
 
-app.get('/scrape', function(req, res){
+app.get('/scrape', function (req, res) {
     var url = 'https://www.endomondo.com/challenges/26556403';
-    request(url, function(error, response, html){
+    request(url, function (error, response, html) {
         console.log('request...');
-        if(!error){
+        if (!error) {
             var $ = cheerio.load(html);
 
             console.log(html);
-            var names = $('td .name').map(function(i, a){
-              return a.text.replace('You', 'Branden Barber');
+            var names = $('td .name').map(function (i, a) {
+                return a.children[0].data.replace('You', 'Branden Barber');
             });
 
-            var kcals = $('.nose').map(function(i, a){
-              return a.innerHTML.replace('&nbsp;kcal', '');
+            var maxLength = 0;
+            for(var i = 0; i < names.length; i++){
+                if (names[i].length > maxLength)
+                    maxLength = names[i].length;
+            };
+            
+            names = names.map(function (i, a) {
+                // Pad names so kcals right align
+                return a + Array(maxLength - a.length + 1).join(' ');
             });
 
-            var arr = scores.map(function(i, score){
-              return names[i] + ' - ' + score;
+            var kcals = $('.nose').map(function (i, a) {
+                return a.children[0].data.replace('&nbsp;kcal', '');
+            });
+
+            var arr = kcals.map(function (i, score) {
+                return names[i] + ' - ' + score;
             });
 
             var list = [];
-            for(var i = 0; i < arr.length; i++)
-            {
-              list.push(arr[i])
+            for (var i = 0; i < arr.length; i++) {
+                list.push(arr[i])
             }
 
             var body = list.join('\r\n');
@@ -37,10 +47,10 @@ app.get('/scrape', function(req, res){
             console.log(secrets.slack.channel);
 
             var params = '?token=' + secrets.slack.token + '&channel=%23' + secrets.slack.channel;
-                var options = {
-                    url: 'https://dontpaniclabs.slack.com/services/hooks/slackbot' + params,
-                    body: body
-                };
+            var options = {
+                url: 'https://dontpaniclabs.slack.com/services/hooks/slackbot' + params,
+                body: "```" + body + "```"
+            };
 
             request.post(options);
         }
