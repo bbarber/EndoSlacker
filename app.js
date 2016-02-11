80f63684e01594c4fdb417a5904b687c71c1492f
bbarber <bbarber@nebraskaglobal.com>
2016-02-10 20:19:00 -0600

    Add start script for heroku

--- app.js
+++ app.js
@@ -4,27 +4,26 @@ var cheerio = require('cheerio');
 var request = require('request');
 var secrets = require('./secrets');
 var app = express();
+var last = "";
 
-app.get('/scrape', function (req, res) {
+app.get('/', function (req, res) {
     var url = 'https://www.endomondo.com/challenges/26556403';
     request(url, function (error, response, html) {
-        console.log('request...');
         if (!error) {
             var $ = cheerio.load(html);
 
-            console.log(html);
             var names = $('td .name').map(function (i, a) {
                 return a.children[0].data.replace('You', 'Branden Barber');
             });
 
             var maxLength = 0;
-            for(var i = 0; i < names.length; i++){
+            for (var i = 0; i < names.length; i++) {
                 if (names[i].length > maxLength)
                     maxLength = names[i].length;
-            };
-            
+            }
+
+            // Pad names so kcals right align
             names = names.map(function (i, a) {
-                // Pad names so kcals right align
                 return a + Array(maxLength - a.length + 1).join(' ');
             });
 
@@ -42,17 +41,21 @@ app.get('/scrape', function (req, res) {
             }
 
             var body = list.join('\r\n');
-            console.log(body);
-            console.log(secrets.slack.token);
-            console.log(secrets.slack.channel);
 
-            var params = '?token=' + secrets.slack.token + '&channel=%23' + secrets.slack.channel;
-            var options = {
-                url: 'https://dontpaniclabs.slack.com/services/hooks/slackbot' + params,
-                body: "```" + body + "```"
-            };
 
-            request.post(options);
+            if (last !== body) {
+                last = body;
+
+                var params = '?token=' + secrets.slack.token + '&channel=%23' + secrets.slack.channel;
+                var options = {
+                    url: 'https://dontpaniclabs.slack.com/services/hooks/slackbot' + params,
+                    body: "```" + body + "```"
+                };
+
+                request.post(options);
+            }
+
+            res.send('<pre>' + body + '</pre>');
         }
     })
 })
