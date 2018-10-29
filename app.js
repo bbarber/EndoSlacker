cb7ce466bcb9bf8599e1d1e31fc870e948e3d89e
Johan Horvius <drhorv@gmail.com>
2018-10-29 15:02:35 +0100

    Added comparision with previous position

--- app.js
+++ app.js
@@ -10,23 +10,28 @@ const writeFile = promisify(fs.writeFile);
 var SLACK_URL_TOKEN = process.env.SLACK_URL_TOKEN;
 var CHALLENGE_ID = process.env.CHALLENGE_ID;
 
-const formatBody = body => "```" + body + "```";
+const formatBody = list => "```" + list.map(item => `${_.padStart(item.position, 2)}: ${item.name} - ${item.score} - ${item.change}`).join("\n") + "```";
 
-const fileName = __dirname + "/last.txt";
+const fileName = __dirname + "/last.json";
 console.log(fileName);
 
 fetchNamesAndScores(CHALLENGE_ID).then(namesAndScores => {
+  console.log(namesAndScores)
+
   const list = namesAndScores.map(
-    ([name, score], index) => `${_.padStart(index + 1, 2)}: ${name} - ${score}`
-  );
-  const body = list.join("\n");
+    ([name, score], index) => ({ name, score, position: index + 1 })
+  )
 
   readFile(fileName, "utf8")
-    .catch(() => "")
+    .catch(() => "[]")
+    .then(raw => JSON.parse(raw))
     .then(last => {
-      if (last !== body) {
+      if (!_.isEqual(last, list)) {
+
+        const comparedList = toComparedList(list, last)
+
         const payload = {
-          text: formatBody(body),
+          text: formatBody(comparedList),
           username: "Endomondo",
           icon_url:
             "https://www.endomondo.com/assets/view/layout/header/assets/ua-header@2x.3ae9f356a186d4064e9a3ee956293bb3.png"
@@ -37,9 +42,29 @@ fetchNamesAndScores(CHALLENGE_ID).then(namesAndScores => {
           body: JSON.stringify(payload)
         };
 
-        return writeFile(fileName, body).then(() => {
+        writeFile(fileName, JSON.stringify(comparedList)).then(() => {
           request.post(options);
         });
       }
     });
 });
+
+toComparedList = (list, last) => {
+  return list.map(p => {
+    if (p.name === '(no name provided)') {
+      return { ...p, change: '-' };
+    }
+    const currPos = p.position;
+    const lastEntryOfPerson = last.find(item => item.name === p.name);
+    if (lastEntryOfPerson) {
+      if (currPos < lastEntryOfPerson.position) {
+        return { ...p, change: '↑' };
+      }
+      else if (currPos > lastEntryOfPerson.position) {
+        return { ...p, change: '↓' };
+      }
+    }
+    return { ...p, change: '-' };
+  });
+}
+
